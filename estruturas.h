563a2508d7cdac9f3c25bec3b6b4acf05e7c1414
RafaMazoliniF <rafael.mazolinif@gmail.com>
2023-09-23 11:20:09 -0300

    add métodos básicos e cria as filas por prioridade e list de pendentes

--- estruturas.h
+++ estruturas.h
@@ -1,51 +1,75 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 
+//* TAREFA
 typedef struct {
-    int dia, mes, ano;
+    int dia;
+    int mes;
+    int ano;
 } Data;
 
-typedef struct {
+typedef struct{
     int codigo;
     char nome[30];
     char projeto[30];
-    Data data_incio;
-    Data data_fim;
-    int status; // 1 => atrasado; 0 => em dia; -1 => pendente;
-    int prioridade; // 1 => alta; 2 => media; 3 => baixa;
+    Data inicio;
+    Data termino;
+    int status;
+    int prioridade;
 } Tarefa;
 
+//*NO
 typedef struct No{
-    struct No * proximo_no;
     Tarefa tarefa;
+    struct No * proximo_no;
 } No;
 
+//*LISTA
+typedef No Lista;
+
+//Cria uma nova lista sendo nula
+Lista * criaLista() {
+    return NULL;
+}
+
+//Se a lista estiver vazia, retorna true
+bool vaziaLista(Lista * lista) {
+    if (lista == NULL) return true;
+    return false;
+}
+
+//Cria um novo nó com uma dada tarefa e o insere na posição 0
+void insereLista(Lista ** lista, Tarefa tarefa) {
+    No * novo_no = (No *) malloc(sizeof(No));
+    novo_no->tarefa = tarefa;
+    novo_no->proximo_no = (*lista);
+    *lista = novo_no;
+}
 
 //*FILA
-typedef struct Fila {
+typedef struct {
     No * inicio;
     No * fim;
 } Fila;
 
-
-//Inicializa uma fila com 0 nos
+//Cria uma nova fila com o início e o fim nulos
 Fila * criaFila() {
     Fila * nova_fila = (Fila *) malloc(sizeof(Fila));
-    nova_fila->inicio = NULL;
     nova_fila->fim = NULL;
+    nova_fila->inicio = NULL;
 
     return nova_fila;
 }
 
-//Retorna se a fila esta vazia
+//Se a fila estiver vazia, retorna true
 bool vaziaFila(Fila * fila) {
     if (fila->inicio == NULL) return true;
     return false;
 }
 
-//Insere uma tarefa no final da fila
+//Cria um novo nó com uma dada tarefa e o insere na fila
 void insereFila(Fila * fila, Tarefa tarefa) {
     No * novo_no = (No *) malloc(sizeof(No));
     novo_no->tarefa = tarefa;
@@ -55,87 +79,8 @@ void insereFila(Fila * fila, Tarefa tarefa) {
         fila->inicio = novo_no;
         novo_no->proximo_no = NULL;
     } else {
-        novo_no->proximo_no = fila->fim;
+        fila->fim->proximo_no = novo_no;
         fila->fim = novo_no;
-    }
-}
-
-//*LISTA
-typedef No Lista;
-
-//Inicia uma lista com 0 nos
-Lista * criaLista() {
-    return NULL;
-}
-
-//Retorna true se a lista estiver vazia e false se não
-bool vaziaLista(Lista * lista) {
-    if (lista == NULL) return true;
-    return false;
-}
-
-//retorna o no na posicao indicada:
-// [-1] -> ultimo no; [nao existe] -> ultimo no; 
-No * getNo(Lista * lista, int posicao) {
-    if (posicao == -1) {
-        if (vaziaLista(lista)) return lista;
-
-        while (lista->proximo_no != NULL) {
-            lista = lista->proximo_no;
-        }
-
-        return lista;
-    }
-
-    else {
-        for (int i = 0; i < posicao; i++) {
-            lista = lista->proximo_no;
-            if (lista->proximo_no == NULL) {
-                return lista;
-            }
-        }
-    }
-
-    return lista;
-}
-
-
-//Insere um no em qualquer posicao da lista:
-// [-1] -> final; [nao existe] -> final;
-void insereLista(Lista ** lista, Tarefa tarefa, int posicao) {
-    No * novo_no = (No *) malloc(sizeof(No));
-
-    if (posicao == 0) {
-        if (vaziaLista(*lista)) {
-            novo_no->tarefa = tarefa;
-            novo_no->proximo_no = NULL;
-            *lista = novo_no;
-        } else {
-            novo_no->tarefa = tarefa;
-            novo_no->proximo_no = *lista;
-            *lista = novo_no;
-        }
-    }
-
-    else if (posicao > 0) {
-        No * no_anterior = getNo(*lista, posicao - 1);
-        novo_no->tarefa = tarefa;
-        novo_no->proximo_no = no_anterior->proximo_no;
-        no_anterior->proximo_no = novo_no;
-    }
-
-    else if (posicao == -1) {
-        if (vaziaLista(*lista)) {
-            *lista = novo_no;
-            novo_no->tarefa = tarefa;
-            novo_no->proximo_no = NULL;
-        }
-
-        else {
-            No * ultimo_no = getNo(*lista, -1);
-            ultimo_no->proximo_no = novo_no;
-            novo_no->proximo_no = NULL;
-            novo_no->tarefa = tarefa;
-        }
+        fila->fim->proximo_no = NULL;
     }
 }
--- main.c
+++ main.c
@@ -1,101 +1,14 @@
 #include "estruturas.h"
 
-void printaDiv() {
-    printf("\n");
-    for (int i = 0; i < 50; i++) printf("=");
-    printf("\n");
-}
-
-Tarefa criaTarefa(int codigo, char nome[], char projeto[], int dia_inicio, int mes_inicio, int ano_inicio, int dia_fim, int mes_fim, int ano_fim, int status, int prioridade);
-void insereTarefa(Tarefa tarefa, Fila * filas[], int * codigo_atual);
-void alteraTarefa(int codigo, Fila * filas[], Lista * pendentes);
-
-int main () {
-    int codigo_atual = 0;
-
-    //Cria as 3 filas por prioridade e coloca-as num vetor
+int main() {
+    //Cria as filas de tarefas por prioridade
     Fila * filas[3];
     for (int i = 0; i < 3; i++) {
-        filas[i] = criaFila();
+        filas[i] = criaFila(); 
     }
 
-    Lista * lista_pendentes = criaLista();
-
-    alteraTarefa(1, filas, lista_pendentes);
+    //Cria lista de pendentes
+    Lista * pendentes = criaLista();
 
     return 0;
 }
-
-//Cria uma nova tarefa e insere as suas informacoes
-Tarefa criaTarefa(int codigo, char nome[], char projeto[], int dia_inicio, int mes_inicio, int ano_inicio, int dia_fim, int mes_fim, int ano_fim, int status, int prioridade) {
-    Tarefa nova_tarefa;
-    nova_tarefa.codigo = codigo;
-    strcpy(nova_tarefa.nome, nome);
-    strcpy(nova_tarefa.projeto, projeto);
-    nova_tarefa.data_incio.dia = dia_inicio;
-    nova_tarefa.data_incio.mes = mes_inicio;
-    nova_tarefa.data_incio.ano = ano_inicio;
-    nova_tarefa.data_fim.dia = dia_fim;
-    nova_tarefa.data_fim.mes = mes_fim;
-    nova_tarefa.data_fim.ano = ano_fim;
-    nova_tarefa.status = status;
-    nova_tarefa.prioridade = prioridade;
-
-    return nova_tarefa;
-}
-
-//Insere uma tarefa na fila de tarefas correta
-void insereTarefa(Tarefa tarefa, Fila * filas[], int * codigo_atual) {
-    insereFila(filas[tarefa.prioridade - 1], tarefa);
-    *codigo_atual++;
-}
-
-Tarefa getTarefa(int codigo, Fila * filas[], Lista * pendentes) {
-    for (int i = 0; i < 3; i++) {
-        No * no_atual = filas[i]->fim;
-        while (no_atual != NULL) {
-            if (no_atual->tarefa.codigo == codigo) return no_atual->tarefa;
-            no_atual = no_atual->proximo_no;
-        }
-    }
-
-    while (pendentes != NULL) {
-        if (pendentes->tarefa.codigo == codigo) return pendentes->tarefa;
-        pendentes = pendentes->proximo_no;
-    }
-
-    Tarefa tarefa_vazia;
-    return tarefa_vazia;
-}
-
-void alteraTarefa(int codigo, Fila * filas[], Lista * pendentes) {
-    printaDiv();
-    printf("AREA DE ALTERACO DE DADOS\n\n");
-    printf("Digite para alterar:\n[1]Nome da tarefa\n[2]Nome do projeto\n[3]Data de inicio\n[4]Data de termino\n[5]Status\n[6]Prioridade\n\n> ");
-
-    int escolha;
-    fflush(stdin);
-    scanf("%d", &escolha);
-
-    Tarefa tarefa = getTarefa(codigo, filas, pendentes);
-
-    switch (escolha) {
-        case 1:
-            printf("\nNovo nome: ");
-            tarefa.nome = gets();
-            break;
-        case 2:
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        default: printf("Escolha inválida. Nenhuma alteracao foi feita.");
-    }
-
-    printaDiv();
-}   
