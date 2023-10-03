32d98d09724c9b529b863fabe9a0cadac36aacd8
Rafael Mazolini Fernandes <rafael.mazolinif@gmail.com>
2023-10-03 19:00:20 -0300

    Organiza o arquivo main e inicia o desenvolvimento da interface.

--- main.c
+++ main.c
@@ -1,13 +1,78 @@
 #include "estruturas.h"
 #include <time.h>
 
-Tarefa * criaTarefa(int * codigo_atual, int hoje[]);
-void cadastraNovaTarefa(Fila * filas[], Lista ** pendentes, Tarefa * tarefa);
-Tarefa * getTarefa(int codigo, Lista * pendentes, Fila * filas[]);
-void editaTarefa(int codigo, Fila * filas[], Lista * pendentes);
+void limpaTela() {
+    #ifdef __linux__
+        system("clear");
+    #elif _WIN32
+        system("cls");
+    #else
+    #endif
+}
+
+void escolha1(Fila * filas[], Lista * pendentes, Lista * concluidas) {
+    limpaTela();
+    printf("Mostrar quais tarefas?\n[1] Todas;\n[2] Somente as ativas;\n[3] Somente as pendentes\n[4] Somente as concluidas;\n[5] Voltar;\n> ");
+    
+    int escolha; 
+    scanf("%d", &escolha);
+
+    switch (escolha) {
+        case 1:
+            limpaTela();
+
+            printf("TAREFAS ATIVAS\n");
+            for (int i = 0; i < 3; i++) {
+                printf("\nPriotidade %d:", i + 1);
+                printaFila(filas[i]);
+            }
+
+            printf("\n\nTAREFAS PENDENTES");
+            printaLista(pendentes);
+            printf("\n\nTAREFAS CONCLUIDAS");
+            printaLista(concluidas);
+            break;
+        case 2:
+            limpaTela();
+
+            printf("TAREFAS ATIVAS\n");
+            for (int i = 0; i < 3; i++) {
+                printf("\nPriotidade %d:", i + 1);
+                printaFila(filas[i]);
+            }
+            break;
+        case 3:
+            limpaTela();
+
+            printf("\n\nTAREFAS PENDENTES");
+            printaLista(pendentes);
+            break;
+        case 4:
+            limpaTela();
+
+            printf("\n\nTAREFAS CONCLUIDAS");
+            printaLista(concluidas);
+            break;
+        case 5:
+        default: 
+        limpaTela();
+        break;
+    }
+}
+
+void escolha2() {
+
+}
+
+void escolha3(){
+
+}
 
 int main() {
+    limpaTela();
+
     int codigo_atual = 0;
+    bool terminou = false;
 
     //DEFINE A DATA ATUAL
     time_t tempo_atual;
@@ -41,148 +106,40 @@ int main() {
     Lista * pendentes = criaLista();
     Lista * concluidas = criaLista();  
 
-    cadastraNovaTarefa(filas, &pendentes, criaTarefa(&codigo_atual, hoje));
-    cadastraNovaTarefa(filas, &pendentes, criaTarefa(&codigo_atual, hoje));
-    cadastraNovaTarefa(filas, &pendentes, criaTarefa(&codigo_atual, hoje));
+    //INICIO DO PROGRAMA
+    printf("===== GERENCIADOR DE TAREFAS =====\n");
+    while (!terminou) {
+        printf("\nDigite um numero para escolher uma acao:\n[1] Mostrar as tarefas existentes; \n[2] Adicionar uma nova tarefa;\n[3] Abrir uma tarefa; \n[4] Fechar;\n> ");
+        
+        int escolha; 
+        fflush(stdin);
+        scanf("%d", &escolha);
+        
+        switch (escolha){
+            case 1:
+                escolha1(filas, pendentes, concluidas);
+                break;
+            case 2:
+                escolha2();
+                break;
+            case 3:
+                escolha3();
+                break;
+            case 4: 
+                terminou = true;
+                break;
+            default: 
+                limpaTela();
+                printf("Escolha invalida.\n");
+                break;
+        }
+    }
 
 
+    printf("\n");
     return 0;
 }
 
 
-Tarefa * criaTarefa(int * codigo_atual, int hoje[]) {
-    
-    Tarefa * nova_tarefa = (Tarefa *) malloc(sizeof(Tarefa));
 
-    printaDiv();
-    printf("AREA DE CRIACAO DE TAREFA\n\n");
 
-    //NOME
-    printf("Digite o nome da tarefa: ");
-    fflush(stdin);
-    fgets(nova_tarefa->nome, 30, stdin);
-    int nome_i = 0;
-    while (nova_tarefa->nome[nome_i] != '\n') nome_i++;
-    nova_tarefa->nome[nome_i] = '\0';
-
-    //PROJETO
-    printf("\nDigite o nome do projeto: ");
-    fflush(stdin);
-    fgets(nova_tarefa->projeto, 30, stdin);
-    int projeto_i = 0;
-    while (nova_tarefa->projeto[projeto_i] != '\n') projeto_i++;
-    nova_tarefa->projeto[projeto_i] = '\0';
-
-    //DATA INICIO
-    printf("\nData de início: %d/%d/%d", hoje[0], hoje[1], hoje[2]);
-    nova_tarefa->inicio.dia = hoje[0];
-    nova_tarefa->inicio.mes = hoje[1];
-    nova_tarefa->inicio.ano = hoje[2];
-
-    //DATA TERMINO
-    printf("\nDigite a data de termino:");
-    printf("\nDia: "); fflush(stdin); scanf("%d", &nova_tarefa->termino.dia);
-    printf("Mes: "); fflush(stdin); scanf("%d", &nova_tarefa->termino.mes);
-    printf("Ano: "); fflush(stdin); scanf("%d", &nova_tarefa->termino.ano);
-
-    //STATUS
-    printf("\nDigite o status da tarefa ([1] => atrasada; [0] => em dia; [-1] => pendente)\n> "); 
-    fflush(stdin); 
-    scanf("%d", &nova_tarefa->status);
-
-    //PRIORIDADE
-    printf("\nDigite a prioridade da tarefa ([1] => Alta; [2] => Media; [3] => Baixa)\n> ");
-    fflush(stdin);
-    scanf("%d", &nova_tarefa->prioridade);
-    getchar();
-
-    //CODIGO
-    nova_tarefa->codigo = *codigo_atual;
-    printf("\nO codigo da tarefa eh: %d", *codigo_atual);
-    printaDiv();
-
-    (*codigo_atual)++;
-
-    return nova_tarefa;
-}
-
-//Recebe uma tarefa e insere em uma das filas ou na lista de pendentes
-void cadastraNovaTarefa(Fila * filas[], Lista ** pendentes, Tarefa * tarefa) {
-    if (tarefa->status == -1) {
-        insereLista(pendentes, tarefa);
-    }
-
-    else {
-        insereFila(filas[tarefa->prioridade - 1], tarefa);
-    }
-}
-
-//retorna a tarefa com o código passado
-Tarefa * getTarefa(int codigo, Lista * pendentes, Fila * filas[]) {
-    while (pendentes != NULL) {
-        if (pendentes->tarefa->codigo == codigo) return pendentes->tarefa;
-        pendentes = pendentes->proximo_no;
-    }
-
-    for (int i = 0; i < 3; i++) {
-        No * no_atual = filas[i]->inicio;
-        while (no_atual != NULL) {
-            if (no_atual->tarefa->codigo == codigo) return no_atual->tarefa;
-            no_atual = no_atual->proximo_no;
-        }
-    }
-
-    return NULL; // Default
-}
-
-//Recebe o codigo de uma tarefa e altera as suas informacoes
-void editaTarefa(int codigo, Fila * filas[], Lista * pendentes) {
-    Tarefa * tarefa = getTarefa(codigo, pendentes, filas);
-    
-    printaDiv();
-    printf("AREA DE EDICAO DE TAREFA\n\n");
-
-    printf("Para editar, digite:\n[0] Nome\n[1] Projeto;\n[2] Data de inicio;\n[3] Data de termino;\n[4] Status;\n[5] Prioridade;\n> ");
-    int escolha;
-    fflush(stdin);
-    scanf("%d", &escolha);
-
-    switch (escolha) {
-        case 0:
-            printf("\n Novo nome: ");
-            fflush(stdin);
-            fgets(tarefa->nome, 30, stdin);
-            break;
-        case 1:
-            printf("\n Novo projeto: ");
-            fflush(stdin);
-            fgets(tarefa->nome, 30, stdin);
-            break;
-        case 2:
-            printf("Nova data de inicio:");
-            printf("\nDia: "); fflush(stdin); scanf("%d", &tarefa->inicio.dia);
-            printf("Mes: "); fflush(stdin); scanf("%d", &tarefa->inicio.mes);
-            printf("Ano: "); fflush(stdin); scanf("%d", &tarefa->inicio.ano);
-            break;
-        case 3:
-            printf("Nova data de termino:");
-            printf("\nDia: "); fflush(stdin); scanf("%d", &tarefa->inicio.dia);
-            printf("Mes: "); fflush(stdin); scanf("%d", &tarefa->inicio.mes);
-            printf("Ano: "); fflush(stdin); scanf("%d", &tarefa->inicio.ano);
-            break;
-        case 4:
-            printf("Novo status: ");
-            fflush(stdin);
-            scanf("%d", &tarefa->status);
-            break;
-        case 5:
-            printf("Nova prioridade: ");
-            fflush(stdin);
-            scanf("%d", &tarefa->prioridade);
-            break;
-        default: 
-            printf("Escolha invalida");
-    }
-
-    printaDiv();
-}
